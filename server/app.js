7db0c74b33df36a19bfdfdc272a5daf096a52442
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-09-05 19:18:54 -0500

    [Feautre/Update] The app.js is set to run only the backend.

--- server/app.js
+++ server/app.js
@@ -4,10 +4,11 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const router = require("./routes");
+require("dotenv").config();
 
 const app = express();
 
-// config
+console.log(process.env.NODE_ENV !== "production");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
@@ -17,16 +18,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(router);
 __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+//if (process.env.NODE_ENV === "production") {
+  //app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
+  //app.get("*", (req, res) => {
+  //  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  //});
+//} else {
   app.get("/", (req, res) => {
     res.send("Server is Running! 🚀");
   });
-}
+//}
 
 module.exports = app;
