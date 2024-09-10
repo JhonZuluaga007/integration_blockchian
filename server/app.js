db4206604740fb2ce2aca47654db4e5c10c7d9d9
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-09-10 10:35:22 -0500

    [Feature/Update] update file app.js

--- server/app.js
+++ server/app.js
@@ -18,16 +18,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(router);
 __dirname = path.resolve();
-//if (process.env.NODE_ENV === "production") {
-  //app.use(express.static(path.join(__dirname, "/frontend/build")));
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  //app.get("*", (req, res) => {
-  //  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  //});
-//} else {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
   app.get("/", (req, res) => {
     res.send("Server is Running! 🚀");
   });
-//}
+}
 
 module.exports = app;
